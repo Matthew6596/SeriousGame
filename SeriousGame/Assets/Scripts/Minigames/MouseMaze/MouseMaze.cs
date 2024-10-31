5b43b0c13dd733fdb35aea29efd23e16e60c4aa4
rft0202 <rftester@email.neit.edu>
2024-10-30 23:11:50 -0400

    Maze Tilemap

---
+++ SeriousGame/Assets/Scripts/Minigames/MouseMaze/MouseMaze.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MouseMaze : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Mouse
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
