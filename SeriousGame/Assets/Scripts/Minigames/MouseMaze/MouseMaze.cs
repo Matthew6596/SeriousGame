3999d2e59d588f6fbbc43eb67e9abb67f36c38d4
rft0202 <114946327+rft0202@users.noreply.github.com>
2024-11-07 13:44:44 -0500

    Maze is functional

--- SeriousGame/Assets/Scripts/Minigames/MouseMaze/MouseMaze.cs
+++ SeriousGame/Assets/Scripts/Minigames/MouseMaze/MouseMaze.cs
@@ -2,16 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
 
 public class MouseMaze : MonoBehaviour
 {
     //PlayerMouse
-    public Vector2 mouseStartingPos = new Vector2(-8, 0);
+    public Vector2 mouseStartingPos = new Vector2(50, 550);
+    Vector2 mousePosition;
 
     // Start is called before the first frame update
     void Start()
     {
         MenuManager.lastMinigame = "MouseMaze";
+        Mouse.current.WarpCursorPosition(mouseStartingPos);
     }
 
     // Update is called once per frame
@@ -30,7 +33,20 @@ public class MouseMaze : MonoBehaviour
         Debug.Log("Collision");
         if (collision.gameObject.GetComponent<MouseFollower>() != null) //if mouse
         {
+            mousePosition = Camera.main.WorldToScreenPoint(PlayerMouse.inst.mousePos);
+            //position.y -= 1;
+
+            //Mouse.current.WarpCursorPosition(position);
+            //PlayerMouse.inst.mousePos = mouseStartingPos;
             Debug.Log("Mouse");
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.GetComponent<MouseFollower>() != null) //if mouse
+        {
+            Mouse.current.WarpCursorPosition(mousePosition);
         }
     }
 }
