e5288c41df385217469013220d7df19ee999c934
rft0202 <rftester@email.neit.edu>
2024-11-21 13:20:28 -0500

    worked on sliding puzzle

--- SeriousGame/Assets/Scripts/Minigames/SlidingPuzzle/PuzzleScript.cs
+++ SeriousGame/Assets/Scripts/Minigames/SlidingPuzzle/PuzzleScript.cs
@@ -5,20 +5,25 @@ using UnityEngine.UIElements;
 
 public class PuzzleScript : MonoBehaviour
 {
+    public bool canWin = false;
     public int numOfPieces;
     public GameObject[] puzzlePieces;
     public Vector2[] positions;
+
+    public Vector2 blankPiece;
 
     // Start is called before the first frame update
     void Start()
     {
         MenuManager.lastMinigame = "SlidingPuzzle";
+        ChooseBlankPiece();
+        //canWin = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(CheckPuzzlePositions())
+        if(CheckPuzzlePositions() && canWin)
         {
             MenuManager.DelayAction(0.6f, () => { MenuManager.Inst.ChangeScene("WinScreen"); });
             Debug.Log("You Win!");
@@ -39,6 +44,13 @@ public class PuzzleScript : MonoBehaviour
         }
 
         return correct;
+    }
+
+    void ChooseBlankPiece()
+    {
+        int index = Random.Range(0, numOfPieces);
+        blankPiece = puzzlePieces[index].transform.localPosition;
+        puzzlePieces[index].GetComponent<SpriteRenderer>().enabled = false;
     }
 
 }
